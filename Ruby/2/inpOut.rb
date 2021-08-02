8f856df3cd602bfd4139556a3a266330aa352789
irahel <isaac_rahel@live.com>
2021-08-02 16:15:00 -0300

    inp out

---
+++ Ruby/2/inpOut.rb
@@ -0,0 +1,13 @@
+#this is a comment
+
+#entry commands
+#print --> dont show a \n
+#puts --> shows a \n
+
+print 'type your name> '
+name = gets.chomp
+
+print 'type your LAST name> '
+l_name = gets.chomp
+
+puts "Hello #{name} #{l_name} !"
---
+++ Ruby/2/sum.rb
@@ -0,0 +1,7 @@
+print 'type a number> '
+number1 = gets.chomp.to_i
+
+print 'type another number> '
+number2 = gets.chomp.to_i
+
+puts "the sum between #{number1} and #{number2} is #{number1+number2}"
