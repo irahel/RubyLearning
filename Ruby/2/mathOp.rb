d7e879df3c0bd634a29757ad80f30aa6066120f1
irahel <isaac_rahel@live.com>
2021-08-02 16:05:59 -0300

    math op

---
+++ Ruby/2/mathOp.rb
@@ -0,0 +1,19 @@
+puts 'sum:'
+puts -5 + 10
+
+puts 'sub:'
+puts 4 - 1
+
+puts 'mult:'
+puts 200 * 13
+
+puts 'div:'
+puts 500 / 5
+
+puts 'mod:'
+puts 999 % 3
+
+puts 'exp:'
+puts 2**9
+
+puts 1+4+5-8*8/9-9+1
